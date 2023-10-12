033a2b93d69f9526024079fb67d215e370c7bae6
djanyreason <djanyreason@gmail.com>
2023-10-12 11:26:54 -0400

    Completes ex. 8.4, adds author param to allBooks
    This commit modifies the allBooks query to include an optional
    author param. This param is a string, and if provided the query
    returns only books which have an author field matching the provided
    param string.

--- library_GraphQL/index.js
+++ library_GraphQL/index.js
@@ -116,7 +116,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -125,7 +125,10 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) return books;
+      return books.filter((book) => book.author === args.author);
+    },
     allAuthors: () => authors
   },
   Author: {
