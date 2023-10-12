5e2a2cb61735f54bcd0badc8de4fc40258971d96
djanyreason <djanyreason@gmail.com>
2023-10-12 10:45:47 -0400

    Completes ex 8.1, adds book/author count queries
    This commit implements two queries, bookCount and authorCount, which
    return the number of books and authors in the data set.
    
    This commit also adds schemas for books and authors, which are not
    necessary for completing this exercise.

--- library_GraphQL/index.js
+++ library_GraphQL/index.js
@@ -98,14 +98,30 @@ let books = [
 */
 
 const typeDefs = `
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String]!
+  }
+
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+  }
+
   type Query {
-    dummy: Int
+    bookCount: Int!
+    authorCount: Int!
   }
 `;
 
 const resolvers = {
   Query: {
-    dummy: () => 0
+    bookCount: () => books.length,
+    authorCount: () => authors.length
   }
 };
 
