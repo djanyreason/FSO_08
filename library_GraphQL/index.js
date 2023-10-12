c5cfb7fcfbb21c386d0ab114a05c29884712e3ea
djanyreason <djanyreason@gmail.com>
2023-10-12 11:21:13 -0400

    Completes ex 8.3, adds author schema & all query
    This commit implements an allAuthors query which returns an array of
    all authors in the collection. It also modifies the author schema
    added in a previous commit to include a 'bookCount' field, which is
    the total number of books by that author in the books collection. A
    resolver for the Author schema is added to resolve the bookCount
    field, as it is not part of the data source for Authors.

--- library_GraphQL/index.js
+++ library_GraphQL/index.js
@@ -110,12 +110,14 @@ const typeDefs = `
     name: String!
     id: ID!
     born: Int
+    bookCount: Int!
   }
 
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `;
 
@@ -123,7 +125,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors
+  },
+  Author: {
+    bookCount: (root) =>
+      books.filter((book) => book.author === root.name).length
   }
 };
 
