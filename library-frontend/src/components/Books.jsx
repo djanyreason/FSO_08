942116115b329fe0c0b2333c3521828c60dda393
djanyreason <djanyreason@gmail.com>
2023-10-16 10:44:45 -0400

    Completes ex 8.9, implements Books view
    This commit implements the Books view using GraphQL via Apollo
    Client. It adds a query ALL_BOOKS in queries.js, which is imported
    into the Books component. The Books component calls this query, and
    once a response is available renders the data.

--- library-frontend/src/components/Books.jsx
+++ library-frontend/src/components/Books.jsx
@@ -1,5 +1,17 @@
+import { useQuery } from '@apollo/client';
+import { ALL_BOOKS } from '../queries';
+
 const Books = () => {
-  const books = [];
+  const result = useQuery(ALL_BOOKS);
+
+  if (result.loading)
+    return (
+      <div>
+        <h2>books</h2>
+        <div>loading...</div>
+      </div>
+    );
+  const books = result.data.allBooks;
 
   return (
     <div>
@@ -12,11 +24,11 @@ const Books = () => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author}</td>
-              <td>{a.published}</td>
+          {books.map((b) => (
+            <tr key={b.id}>
+              <td>{b.title}</td>
+              <td>{b.author}</td>
+              <td>{b.published}</td>
             </tr>
           ))}
         </tbody>
