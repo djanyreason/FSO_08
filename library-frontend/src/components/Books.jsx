30325dc3834a19dfb97c2ae6b0dbe48932cff6fd
djanyreason <djanyreason@gmail.com>
2023-10-18 11:52:18 -0400

    Completes ex. 8.19, adds genre filter to books
    This commit adds a set of buttons to the Books component, one for
    each genre present in the books list as well as an 'all genres'
    button. Clicking a button filters the books list to only show books
    of that genre. The list of buttons is generated using the SET
    object.

--- library-frontend/src/components/Books.jsx
+++ library-frontend/src/components/Books.jsx
@@ -1,8 +1,10 @@
 import { useQuery } from '@apollo/client';
+import { useState } from 'react';
 import { ALL_BOOKS } from '../queries';
 
 const Books = () => {
   const result = useQuery(ALL_BOOKS);
+  const [genre, setGenre] = useState('all genres');
 
   if (result.loading)
     return (
@@ -13,10 +15,15 @@ const Books = () => {
     );
   const books = result.data.allBooks;
 
+  const genres = [
+    ...new Set(books.reduce((joined, book) => joined.concat(book.genres), [])),
+    'all genres'
+  ];
+
   return (
     <div>
       <h2>books</h2>
-
+      in {genre === 'all genres' ? '' : 'genre'} <strong>{genre}</strong>
       <table>
         <tbody>
           <tr>
@@ -24,15 +31,26 @@ const Books = () => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((b) => (
-            <tr key={b.id}>
-              <td>{b.title}</td>
-              <td>{b.author.name}</td>
-              <td>{b.published}</td>
-            </tr>
-          ))}
+          {books
+            .filter(
+              (b) => genre === 'all genres' || b.genres.indexOf(genre) >= 0
+            )
+            .map((b) => (
+              <tr key={b.id}>
+                <td>{b.title}</td>
+                <td>{b.author.name}</td>
+                <td>{b.published}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
+      <div>
+        {genres.map((g) => (
+          <button key={g} onClick={() => setGenre(g)} autoFocus>
+            {g}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
